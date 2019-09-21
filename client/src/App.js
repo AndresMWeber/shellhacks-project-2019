20db39f0902fa3d7c8b715a752bb3896d1d05823
Adrian <adrianivsdk@gmail.com>
2019-09-21 16:52:26 -0400

    React component boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Map from './components/Map'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+      <Map />
     </div>
   );
 }
