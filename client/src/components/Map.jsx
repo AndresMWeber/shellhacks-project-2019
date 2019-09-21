66ee580062e0cb94ca81e0f5e905a68c68b72101
Adrian <adrianivsdk@gmail.com>
2019-09-21 16:47:59 -0400

    Added component boilerplate

---
+++ client/src/components/Map.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+class Map extends React.Component {
+    state = {
+        h1: "WHAZUPPPPP",
+    }
+    render() {
+        return <h1>Hello, {this.state.h1}</h1>;
+    }
+}
+
+export default Map;
